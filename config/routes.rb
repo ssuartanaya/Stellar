2c98ed70f81ff11c76b772e3c4bb9f50f10ddd8f
mayudiputra <putrammayudi@gmail.com>
2018-01-25 11:08:29 +0800

    index 15% + link to Profile Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :videos
+  resources :users, only: [:show]
   devise_for :users
-  get 'dashboard/index'
-  root to: 'dashboard#index' 
+  
+  get 'users/:id' => 'users#show'
+  root 'videos#index' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
