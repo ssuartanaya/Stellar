778350625fbfdcb912d7231ff1976a08c5677a82
mayudiputra <putrammayudi@gmail.com>
2018-01-26 17:22:47 +0800

    index 50%, show page 20%

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :videos
   resources :users, only: [:show]
-  devise_for :users
   
+  
+  root 'videos#index'
   get 'users/:id' => 'users#show'
-  root 'videos#index' 
+   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
